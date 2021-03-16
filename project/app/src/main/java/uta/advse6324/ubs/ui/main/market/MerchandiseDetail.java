65aded93facfbde828e40540b0f546c57c6a5bd1
kuangzhehuangjian <33214390+kuangzhehuangjian@users.noreply.github.com>
2021-03-16 12:50:43 +0800

    Merchandise Detail框架

---
+++ project/app/src/main/java/uta/advse6324/ubs/ui/main/market/MerchandiseDetail.java
@@ -0,0 +1,13 @@
+package uta.advse6324.ubs.ui.main.market;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import uta.advse6324.ubs.R;
+
+public class MerchandiseDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_merchandise_detail);
+    }
+}
