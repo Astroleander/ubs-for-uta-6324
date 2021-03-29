03de55a4f5804131a4ed72e4b9275da55af7aa60
Astroleander <astroleander@gmail.com>
2021-03-29 21:45:57 +0800

    [sp3] merchandise detail tag

--- project/app/src/main/java/uta/advse6324/ubs/ui/main/market/MerListAdapter.java
+++ project/app/src/main/java/uta/advse6324/ubs/ui/main/market/MerListAdapter.java
@@ -93,7 +93,7 @@ public class MerListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), MerchandiseDetail.class);
-                intent.putExtra("INFORMATION", merchandiseList.get(position));
+                intent.putExtra("Merchandise", merchandiseList.get(position));
                 view.getContext().startActivity(intent);
                 Toast.makeText(view.getContext(), "Click item" + position, Toast.LENGTH_SHORT).show();
             }
