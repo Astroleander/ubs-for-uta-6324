8e1c163de0cdaf1fc90be5663915056353295752
longclothe <504429164@qq.com>
2021-04-27 22:32:23 +0800

    收件箱 发件箱

--- project/app/src/main/java/uta/advse6324/ubs/ui/main/message/MessageFragment.java
+++ project/app/src/main/java/uta/advse6324/ubs/ui/main/message/MessageFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,6 +32,7 @@ import uta.advse6324.ubs.db.MesDBHelper;
 import uta.advse6324.ubs.pojo.Merchandise;
 import uta.advse6324.ubs.pojo.Message;
 import uta.advse6324.ubs.pojo.User;
+import uta.advse6324.ubs.ui.ReleaseNewClub.ReleaseNewClub;
 import uta.advse6324.ubs.ui.main.market.MarketViewModel;
 import uta.advse6324.ubs.ui.main.market.MerListAdapter;
 import uta.advse6324.ubs.ui.main.market.Sell_Activity;
@@ -83,6 +85,9 @@ public class MessageFragment extends Fragment {
     private FloatingActionButton fabMcd;
     private User user;
 
+    private Button send;
+    private Button receive;
+
     private  MesDBHelper messageDBHelper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -90,9 +95,14 @@ public class MessageFragment extends Fragment {
         messageViewModel = new ViewModelProvider(this).get(MessageViewModel.class);
         root = inflater.inflate(R.layout.fragment_message, container, false);
         user = (User) getActivity().getIntent().getSerializableExtra(LOGIN_USER_INFO);
+        send = root.findViewById(R.id.send_button);
+        receive = root.findViewById(R.id.reveive_button);
 
 
         initView();
+        initSearch();
+        initSend();
+        initReceive();
 
         return root;
     }
@@ -102,6 +112,8 @@ public class MessageFragment extends Fragment {
         super.onResume();
         Log.d("MessageFragment", "[onResume]");
         list = root.findViewById(R.id.list);
+        send = root.findViewById(R.id.send_button);
+        receive = root.findViewById(R.id.reveive_button);
         messageViewModel.getList().observe(getViewLifecycleOwner(), new Observer<ArrayList<Message>>() {
             @Override
             public void onChanged(@Nullable ArrayList<Message> arr) {
@@ -119,6 +131,90 @@ public class MessageFragment extends Fragment {
 
                 list.setAdapter(new MesListAdapter(arr, user));
                 list.setLayoutManager(new LinearLayoutManager(root.getContext()));
+            }
+        });
+    }
+    private void initSend() {
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+//                    User user = (User) getActivity().getIntent().getSerializableExtra(LOGIN_USER_INFO);
+//                    //TODO
+//                    Intent intent;
+//                    intent = new Intent(getActivity(), ReleaseNewClub.class);
+//                    intent.putExtra(LOGIN_USER_INFO, user);
+//                    startActivity(intent);
+                    ArrayList<Message> all = messageViewModel.getList().getValue();
+                    ArrayList<Message> result = new ArrayList<>();
+                    for (Message p: all) {
+                        if (p.getSend().contains(user.getId())) {
+                            result.add(p);
+                        }
+                    }
+                    list.setAdapter(new MesListAdapter(result, user));
+
+
+                } catch (Exception e) {
+//                    Log.e("EROERROR", "onClick: ", e);
+//                    Toast.makeText(root.getContext(), "Load user info failed, try login again", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    private void initReceive() {
+        receive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+//                    User user = (User) getActivity().getIntent().getSerializableExtra(LOGIN_USER_INFO);
+//                    //TODO
+//                    Intent intent;
+//                    intent = new Intent(getActivity(), ReleaseNewClub.class);
+//                    intent.putExtra(LOGIN_USER_INFO, user);
+//                    startActivity(intent);
+                    ArrayList<Message> all = messageViewModel.getList().getValue();
+                    ArrayList<Message> result = new ArrayList<>();
+                    for (Message p: all) {
+                        if (p.getReceive().contains(user.getId())) {
+                            result.add(p);
+                        }
+                    }
+                    list.setAdapter(new MesListAdapter(result, user));
+
+
+                } catch (Exception e) {
+//                    Log.e("EROERROR", "onClick: ", e);
+//                    Toast.makeText(root.getContext(), "Load user info failed, try login again", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    private void initSearch() {
+        SearchView searchView = root.findViewById(R.id.search);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                if (s.length() < 1) {
+                    return false;
+                }
+                ArrayList<Message> all = messageViewModel.getList().getValue();
+                ArrayList<Message> result = new ArrayList<>();
+                for (Message p: all) {
+                    if (p.getSend().contains(s) || p.getReceive().contains(s) ) {
+                        result.add(p);
+                    }
+                }
+                list.setAdapter(new MesListAdapter(result, user));
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                if (s.length() < 1) {
+                    list.setAdapter(new MesListAdapter(messageViewModel.getList().getValue(), user));
+                }
+                return false;
             }
         });
     }
