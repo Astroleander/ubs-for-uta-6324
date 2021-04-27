761383a90562a27a96fd69ba2a52000bdd689fc7
llw <841027840@qq.com>
2021-04-27 17:42:51 +0800

    [sp5]Message Detail

--- project/app/src/main/java/uta/advse6324/ubs/ui/main/message/MesDetail.java
+++ project/app/src/main/java/uta/advse6324/ubs/ui/main/message/MesDetail.java
@@ -1,5 +1,7 @@
 package uta.advse6324.ubs.ui.main.message;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -9,18 +11,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import uta.advse6324.ubs.R;
 
-public class MesDetail extends AppCompatActivity {
+import uta.advse6324.ubs.db.ClubDBHelper;
+import uta.advse6324.ubs.db.ClubMemberDBHelper;
+import uta.advse6324.ubs.db.MesDBHelper;
+import uta.advse6324.ubs.pojo.Club;
+import uta.advse6324.ubs.pojo.ClubMember;
+import uta.advse6324.ubs.pojo.Message;
+import uta.advse6324.ubs.pojo.User;
 
+public class MesDetail extends AppCompatActivity {
+    private TextView tv_time;
+    private TextView tv_send;
+    private TextView tv_content;
+    private Button bt_back;
+    private Button bt_delete;
+
+    private Message message;
+    private User user;
+    private MesDBHelper mesDBHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mes_detail);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        this.initView();
+        this.submitBack();
 
+        //???
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,4 +53,52 @@ public class MesDetail extends AppCompatActivity {
             }
         });
     }
+
+    private void submitBack() {
+        mesDBHelper = new MesDBHelper(this);
+        bt_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                finish();
+            }
+        });
+        bt_delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+
+                new AlertDialog.Builder(view.getContext())
+                        .setTitle("Confirm delete the message?")
+                        .setMessage("Confirm delete the message?")
+                        .setIcon(R.drawable.ic_baseline_warning_24)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                 mesDBHelper.delete(new Message(message.getTime(), message.getSend(),message.getReceive(),message.getContent(),message.isRead_status()));//这里可能有问题。
+                                finish();
+
+                            }
+                        })
+                        .setNeutralButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {}
+                        })
+                        .show();
+            }
+        });
+
+    }
+    private void initView() {
+        tv_time = findViewById(R.id.club_detail_nametv);
+        tv_send = findViewById(R.id.club_detail_introtv);
+        tv_content = findViewById(R.id.club_detail_categorytv);
+        bt_back = findViewById(R.id.club_detail_back);
+        bt_delete = findViewById(R.id.club_detail_Join_Club);
+
+        tv_time.setText(message.getTime());
+        tv_send.setText(message.getSend());
+        tv_content.setText(message.getContent());
+
+    }
 }
+
